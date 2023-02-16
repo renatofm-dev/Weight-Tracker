b2b1894b2ae804de0918a5da505cb337bc9328f8
Renato Martins <rmartins6694@gmail.com>
2023-02-16 12:55:38 -0300

    add some const in app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, shallowRef, computed, watch, nextTick } from "vue";
+import chart from "chart.js/auto";
+
+const weights = ref([]);
+
+const weightChartEl = ref(null);
+
+const wheightChart = shallowRef(null);
+
+const weightInput = ref(60.0)
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Hello World</h1>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
